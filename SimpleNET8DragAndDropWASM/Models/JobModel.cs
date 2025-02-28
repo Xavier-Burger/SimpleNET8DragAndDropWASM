486a0e93d5228fbe57e60607869b9cbc61cf8e76
Xavier-Burger <xavier.burger@au.rlb.com>
2025-02-28 15:24:44 +1100

    Initial commit

---
+++ SimpleNET8DragAndDropWASM/Models/JobModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleNET8DragAndDropWASM.Models
+{
+    public class JobModel
+    {
+        public int Id { get; set; }
+        public JobStatuses Status { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime LastUpdated { get; set; }
+    }
+
+    public enum JobStatuses
+    {
+        Todo,
+        Started,
+        Completed
+    }
+}
